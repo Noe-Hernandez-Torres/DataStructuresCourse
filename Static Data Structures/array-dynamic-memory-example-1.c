d7435ad052146c2ce2259010c9683e6973a9590a
Julio  Hernandez <jnht2000@gmail.com>
2025-08-22 11:53:10 -0600

    pointer and array examples

---
+++ Static Data Structures/array-dynamic-memory-example-1.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int n;
+    
+    printf("\nPlease, enter the amount of elements in the array: ");
+    scanf("%d",&n);
+    printf("\n");
+    
+    int *arrayInts = (int *) malloc (n * sizeof(int));
+    
+    if(arrayInts == NULL) {
+        printf("\n Fail to obtain the required memory!\n");
+        return -1;
+    }
+    
+    printf("\n Memory successfully obtained\n");
+    
+    printf("\n Please, enter the %d ints separated by space: ",n);
+    
+    for(int i = 0; i < n; i++)
+        scanf("%d", &arrayInts[i]); //pointer seen as a conventional arrays
+    
+    printf("\n Here your %d ints (in reverse order): ",n);
+    
+    for(int i = n-1; i >= 0; i--)
+        printf("%d",arrayInts[i]);
+    
+    
+    free(arrayInts);
+    
+    return 0;
+}
---
+++ Static Data Structures/pointers-example-2.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+int main()
+{
+    int t = 32;
+    int *ptrInt;
+
+    printf("\n t's address: %p",&t);
+    printf("\n t's value: %d", t);
+    
+    //address cannot be changed
+    //it is assigned by the OS
+    
+    t = 12;
+    ptrInt = &t;
+    
+    printf("\n t's address: %p", ptrInt);
+    printf("\n t's value: %d\n", *ptrInt);
+    return 0;
+}
